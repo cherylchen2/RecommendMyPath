5de49d571c8d78127751399b70f065802c067a31
cherylchen2 <cheryl.pl.chen@hotmail.com>
2020-02-16 08:21:27 -0500

    Update userpref.py
    Implemented new functionality for travelling

--- userpref.py
+++ userpref.py
@@ -17,11 +17,34 @@ class User:
     shopping: Dict
     in_home_country: bool
 
-    def __init__(self, food, arts, entertainment, shopping):
+    def __init__(self, food, arts, entertainment, shopping, in_home_country):
         self.food = food
         self.arts = arts
         self.entertainment = entertainment
         self.shopping = shopping
+        self.in_home_country = in_home_country
+
+    def otherCountryPref(self, food, arts, entertainment, shopping):
+        if food != "":
+            foodList = food.split()
+            for f in foodList:
+                self.food[f] = ['T',0] 
+        if arts != "":
+            artsList = arts.split()
+            for f in artsList:
+                self.arts[f] = ['T',0] 
+        if entertainment != "":
+            entertainmentList = entertainment.split()
+            for f in entertainmentList:
+                self.entertainment[f] = ['T',0] 
+        if shopping != "":
+            shopList = shopping.split()
+            for f in shopList:
+                self.shopping[f] = ['T',0] 
+        print(self.food)
+        print(self.arts)
+        print(self.entertainment)
+        print(self.shopping)
 
 
 
